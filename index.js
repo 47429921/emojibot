fc22c9a89e1e479a1d21de7dcf1d9e6d6a36bc22
47429921 <43491074+47429921@users.noreply.github.com>
2019-12-22 10:31:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 ﻿const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require("./config.json");
+const DBL = require("dblapi.js");
+const dbl = new DBL('Your top.gg token', client);
 
+// Optional events
+dbl.on('posted', () => {
+  console.log('Server count posted!');
+});
+
+dbl.on('error', e => {
+ console.log(`Oops! ${e}`);
+});
 
 client.on('ready', () => {
     console.log(`登入成功 ${client.user.tag}`);
