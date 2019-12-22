2f43ce0e6ae07dd4779c0353ee0ed28e40b49a6e
47429921 <43491074+47429921@users.noreply.github.com>
2019-12-22 09:49:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -317,11 +317,7 @@ if (!message.member.hasPermission("KICK_MEMBERS")) {
       .catch(error => message.reply(`Sorry,We have an error. `));
   }
 
-    if (command === "addemoji") {
-        const sm = args.join(" ");
-        client.channels.get("650257003192516630").send(`${message.author.username} give emoji: ${sm}`);
-	message.channel.send(`Your emoji we will add Thank you!`)
-    }
+    
  if (command === "servers") {
 	 message.channel.send(`Working on ${client.guilds.size} servers`);
  }
