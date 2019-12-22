eb5e1629bcb3cc0e6cb25d2260618fd949027b1b
47429921 <43491074+47429921@users.noreply.github.com>
2019-12-22 22:25:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -232,6 +232,9 @@ if (command === "say") {
     // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
     // To get the "message" itself we join the `args` back into a string with spaces: 
     const sayMessage = args.join(" ");
+if (!message.member.hasPermission("MANAGE_MESSAGES")) {
+      return message.reply(`You don't have permission. If you think this is wrong please report bug at suppot server. `);
+    }
     // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
     message.delete().catch(O_o => { });
     // And we get the bot to say the thing: 
