8a8d397df7ed38611f62bb86de52e9d27be97f2d
47429921 <43491074+47429921@users.noreply.github.com>
2020-01-23 16:57:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ dbl.webhook.on('ready', hook => {
   console.log(`Webhook running at http://${hook.hostname}:${hook.port}${hook.path}`);
 });
 dbl.webhook.on('vote', vote => {
-  console.log(`User with ID ${vote.user} just voted!`);
+  message.channel.get('655236227947036702').send(`User with ID ${vote.user} just voted!`);
 });
 
 client.on('ready', () => {
