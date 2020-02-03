8a76f8dd8338987d45337eb0839bd2a41ed76cba
47429921 <43491074+47429921@users.noreply.github.com>
2020-02-03 14:21:06 +0800

    Update index.js

--- index.js
+++ index.js
@@ -467,7 +467,7 @@ if (!message.member.hasPermission("KICK_MEMBERS")) {
 if(command === "nameguild") {
   let index = 1;
   client.guilds.forEach(g => {
-    console.log(index,g,name)
+    message.auther.send(index,g,name)
     index++;
   })
 }
