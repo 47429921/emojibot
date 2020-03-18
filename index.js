ef1cff57bfda72590381f56719e560de6c52368e
47429921 <43491074+47429921@users.noreply.github.com>
2020-03-18 20:27:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ mongoose.connect('mongo "mongodb+srv://emojibot-hurgt.gcp.mongodb.net/test"  --u
 
 // Optional events
 dbl.on('posted', () => {
-  console.log('服務器數量已發布!');
+  console.log('服務器數量已發布.');
 })
 
 dbl.on('error', e => {
