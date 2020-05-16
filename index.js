1fa5fcab348e183f50c6953d926d53fb99fa8ff6
47429921 <43491074+47429921@users.noreply.github.com>
2020-05-16 17:43:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ client.on('ready', async () => {
   client.user.setActivity(status, { type: 'WATCHING'});
   }, 20000);
  
- await client.guilds.keyArray().forEach(id => {
+ /*await client.guilds.keyArray().forEach(id => {
 	 Config.findone({
 		 guildID: id
 	 }, (guild) => {
@@ -72,7 +72,7 @@ client.on('ready', async () => {
 			 return newconfig.save();
 			 }
 	 }});
- });
+ }); */
 });
 /*setInterval(function() {
   POST https://bots.ondiscord.xyz/bot-api/bots/650246586445594625/guilds HTTP/1.1
