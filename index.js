e652d32dd037ae3388ea29e3ec2b1a148933ca0a
47429921 <43491074+47429921@users.noreply.github.com>
2020-06-05 20:20:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -518,14 +518,15 @@ client.on("message", async message => {
 	return	message.reply('你需要選擇 True 或 false.');
 		}
 		
-		if(set === "true") {
+		if(set == "true") {
+			
 		  const data = new Uint8Array(Buffer.from('Hello Node.js'));
 fs.writeFile('setting.json', data, (err) => {
   if (err) throw err;
   console.log('The file has been saved!');
 });
-			
-		};
+		};	
+		
 		if(set === "false") {
 	return	message.reply('Test no but good');
 		}
